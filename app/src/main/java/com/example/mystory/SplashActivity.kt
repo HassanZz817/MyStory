5a874babe1c102ed1cca990f3f0e9cba23dc43aa
HASAN AL-BAR <tfshan05@gmail.com>
2022-09-18 17:35:13 +0300

    SplashActivity.kt + Empty LoginActivity.kt

---
+++ app/src/main/java/com/example/mystory/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.mystory
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+
+        movetoLoginScreen()
+    }
+    private fun movetoLoginScreen(){
+
+
+        // make delay
+        Handler(Looper.myLooper()!!).postDelayed({
+            finish()
+                val i:Intent = Intent(this,
+                LoginActivity::class.java)
+            startActivity(i)
+        },2000)
+    }
+}
