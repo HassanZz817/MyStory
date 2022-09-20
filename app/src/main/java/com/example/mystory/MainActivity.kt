01e7afcc46c91f24f1883bb6aed1412f783db56c
HASAN AL-BAR <tfshan05@gmail.com>
2022-09-20 15:23:10 +0300

    add menu_drawer.xml with the home.xml and logout.xml to the activity_main.xml("drawerLayout)"

--- app/src/main/java/com/example/mystory/MainActivity.kt
+++ app/src/main/java/com/example/mystory/MainActivity.kt
@@ -2,11 +2,17 @@ package com.example.mystory
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.MenuItem
 import android.widget.TextView
+import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
+import androidx.drawerlayout.widget.DrawerLayout
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var textViewEmail:TextView
+    lateinit var drawerLayout: DrawerLayout
+    lateinit var toolbarView: Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -14,9 +20,28 @@ class MainActivity : AppCompatActivity() {
 
         val email = intent.getStringExtra("username")
         connectViews()
-        textViewEmail.text = email
+        setSupportActionBar(toolbarView)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+
+        setupDrawer()
     }
     private fun connectViews(){
-        textViewEmail = findViewById(R.id.tvEmail)
+
+        drawerLayout = findViewById(R.id.drawer)
+        toolbarView = findViewById(R.id.toolbar)
+    }
+    private fun setupDrawer(){
+        val toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when(item.itemId){
+            android.R.id.home ->{
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            } else -> true
+        }
     }
 }
