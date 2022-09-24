14009a0462cfc59e7a7eef6de63226dd2fca520f
HASAN AL-BAR <77029890+HassanZz817@users.noreply.github.com>
2022-09-24 20:03:10 +0300

    adding the storiesRecyclerView View to activity_main.xml + connecting it to MainActivity.kt

--- app/src/main/java/com/example/mystory/MainActivity.kt
+++ app/src/main/java/com/example/mystory/MainActivity.kt
@@ -9,6 +9,8 @@ import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.Recycler
 import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
@@ -16,6 +18,7 @@ class MainActivity : AppCompatActivity() {
     lateinit var drawerLayout: DrawerLayout
     lateinit var toolbarView: Toolbar
     lateinit var navigationView: NavigationView
+    lateinit var recyclerView: RecyclerView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -35,6 +38,7 @@ class MainActivity : AppCompatActivity() {
         drawerLayout = findViewById(R.id.drawer)
         toolbarView = findViewById(R.id.toolbar)
         navigationView = findViewById(R.id.navView)
+        recyclerView = findViewById(R.id.storiesRecyclerView)
     }
     private fun setupDrawer(){
         val toggle = ActionBarDrawerToggle(this,drawerLayout,R.string.open,R.string.close)
