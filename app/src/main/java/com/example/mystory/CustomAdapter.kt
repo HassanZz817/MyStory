459b0c8d5ed5983a1d4cea480e3fed420fd86074
HASAN AL-BAR <77029890+HassanZz817@users.noreply.github.com>
2022-09-28 13:37:11 +0300

    finishing CustomAdapter.kt and make it inherent  RecyclerView.Adapter + defining class of type Story + call out the functions of that class and make changes when needed made antoher calss insiade of the that class and use custom_layout.xml

--- app/src/main/java/com/example/mystory/CustomAdapter.kt
+++ app/src/main/java/com/example/mystory/CustomAdapter.kt
@@ -1,12 +1,14 @@
 package com.example.mystory
 
 import android.content.Context
+import android.graphics.Color
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.cardview.widget.CardView
 import androidx.recyclerview.widget.RecyclerView
+import kotlin.random.Random
 
 
 class CustomAdapter(val storiesList:ArrayList<Story>, val context:Context): RecyclerView.Adapter<CustomAdapter.DataHolder>() {
@@ -20,19 +22,32 @@ class CustomAdapter(val storiesList:ArrayList<Story>, val context:Context): Recy
         holder.storyTittle.text = story.title
         holder.subtitle.text = story.subtitle
         holder.storyLetter.text = story.title[0].toString()
+        generateColors(holder,position)
+
 
     }
+
+
+    private fun generateColors(holder: DataHolder, position: Int){
+        val r = java.util.Random()
+        val red = r.nextInt(255+position)
+        val green = r.nextInt(255-position+1)
+        val blue = r.nextInt(255-position+1)
+        holder.cardViewCircle.setCardBackgroundColor(Color.rgb(red, green, blue))
+    }
+
 
     override fun getItemCount(): Int {
-        TODO("Not yet implemented")
+        return storiesList.size
     }
+
+
 
 
     class DataHolder(item:View): RecyclerView.ViewHolder(item){
         val storyTittle:TextView = item.findViewById(R.id.mainText)
         val subtitle:TextView = item.findViewById(R.id.subtitle)
-
         val storyLetter:TextView = item.findViewById(R.id.tvStoryLetter)
-        val cardViewLetter:CardView = item.findViewById(R.id.circle_view)
+        val cardViewCircle:CardView = item.findViewById(R.id.circle_view)
     }
 }
